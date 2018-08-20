c1ff4a34231682197b02b88cf0f4279523886b4e
daoxuan <daoxuan@pinduoduo.com>
2018-08-20 21:29:33 +0800

    upgrade rxjava version -> 2.2.0

--- src/main/java/cn/dubby/rxjava/demo/helloworld/HelloWorld.java
+++ src/main/java/cn/dubby/rxjava/demo/helloworld/HelloWorld.java
@@ -10,7 +10,6 @@ public class HelloWorld {
     public static void helloV1(String... names) {
         Observable.fromArray(names).subscribe(new Consumer<String>() {
 
-            @Override
             public void accept(String s) throws Exception {
                 System.out.println("Hello " + s + "!");
             }
@@ -21,22 +20,18 @@ public class HelloWorld {
     public static void helloV2(String... names) {
         Observable.fromArray(names).subscribe(new Observer<String>() {
 
-            @Override
             public void onSubscribe(Disposable d) {
                 System.out.println("onSubscribe");
             }
 
-            @Override
             public void onNext(String s) {
                 System.out.println("onNext: Hello " + s + "!");
             }
 
-            @Override
             public void onError(Throwable e) {
                 System.out.println("onError");
             }
 
-            @Override
             public void onComplete() {
                 System.out.println("onComplete");
             }
